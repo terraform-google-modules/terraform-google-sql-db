da2612d58f5f31ac5b103a2f77528166cccd61dd
Dan Isla <disla@google.com>
2017-09-12 14:47:20 -0700

    initial import

---
+++ main.tf
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+resource "google_sql_database_instance" "master" {
+  name             = "${var.name}"
+  project          = "${var.project}"
+  region           = "${var.region}"
+  database_version = "${var.database_version}"
+
+  settings {
+    tier                        = "${var.tier}"
+    activation_policy           = "${var.activation_policy}"
+    authorized_gae_applications = ["${var.authorized_gae_applications}"]
+    disk_autoresize             = "${var.disk_autoresize}"
+    backup_configuration        = ["${var.backup_configuration}"]
+    ip_configuration            = ["${var.ip_configuration}"]
+    location_preference         = ["${var.location_preference}"]
+    maintenance_window          = ["${var.maintenance_window}"]
+  }
+
+  replica_configuration = ["${var.replica_configuration}"]
+}
+
+resource "google_sql_database" "default" {
+  name      = "${var.db_name}"
+  project   = "${var.project}"
+  instance  = "${google_sql_database_instance.master.name}"
+  charset   = "${var.db_charset}"
+  collation = "${var.db_collation}"
+}
+
+resource "random_id" "user-password" {
+  byte_length = 8
+}
+
+resource "google_sql_user" "default" {
+  name     = "${var.user_name}"
+  project  = "${var.project}"
+  instance = "${google_sql_database_instance.master.name}"
+  host     = "${var.user_host}"
+  password = "${var.user_password == "" ? random_id.user-password.hex : var.user_password}"
+}
