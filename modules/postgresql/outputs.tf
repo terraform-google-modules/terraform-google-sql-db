57a0c5e26c45e342f961c67a0fb3a456f5013c18
Kunpei Sakai <kunpei@google.com>
2019-01-11 17:14:07 +0900

    change "master" to "default"

--- modules/postgresql/outputs.tf
+++ modules/postgresql/outputs.tf
@@ -15,35 +15,40 @@
  */
 
 // Master
-output "master_instance_first_ip_address" {
-  value       = "${google_sql_database_instance.master.ip_address}"
-  description = "The first IPv4 address of the addesses assigned for the master instance"
+output "instance_address" {
+  value       = "${google_sql_database_instance.default.ip_address}"
+  description = "The IPv4 addesses assigned for the master instance"
 }
 
-output "master_instance_connection_name" {
-  value       = "${google_sql_database_instance.master.connection_name}"
+output "instance_first_ip_address" {
+  value       = "${google_sql_database_instance.default.first_ip_address}"
+  description = "The first IPv4 address of the addresses assigned."
+}
+
+output "instance_connection_name" {
+  value       = "${google_sql_database_instance.default.connection_name}"
   description = "The connection name of the master instance to be used in connection strings"
 }
 
-output "master_instance_self_link" {
-  value       = "${google_sql_database_instance.master.self_link}"
+output "instance_self_link" {
+  value       = "${google_sql_database_instance.default.self_link}"
   description = "The URI of the master instance"
 }
 
-output "master_instance_server_ca_cert" {
-  value       = "${google_sql_database_instance.master.server_ca_cert}"
+output "instance_server_ca_cert" {
+  value       = "${google_sql_database_instance.default.server_ca_cert}"
   description = "The CA certificate information used to connect to the SQL instance via SSL"
 }
 
-output "master_instance_service_account_email_address" {
-  value       = "${google_sql_database_instance.master.service_account_email_address}"
+output "instance_service_account_email_address" {
+  value       = "${google_sql_database_instance.default.service_account_email_address}"
   description = "The service account email address assigned to the master instance"
 }
 
 // Replicas
-output "replicas_instance_first_ip_addresses" {
+output "replicas_instance_ip_addresses" {
   value       = ["${google_sql_database_instance.replicas.*.ip_address}"]
-  description = "The first IPv4 addresses of the addesses assigned for the replica instances"
+  description = "The IPv4 addresses assigned for the replica instances"
 }
 
 output "replicas_instance_connection_names" {
