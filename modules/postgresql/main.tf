80fb712a55e1d1d2e988c5d5a24613297c3994b4
Mirko Montanari <mirko.montanari@gmail.com>
2019-05-21 09:18:04 -0700

    Fixed the ip_configuration regression.

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -14,6 +14,15 @@
  * limitations under the License.
  */
 
+locals {
+  ip_configuration_enabled = "${length(keys(var.ip_configuration)) > 0 ? true : false}"
+
+  ip_configurations = {
+    enabled  = "${list(var.ip_configuration)}"
+    disabled = "${list()}"
+  }
+}
+
 resource "google_sql_database_instance" "default" {
   project          = "${var.project_id}"
   name             = "${var.name}"
@@ -26,7 +35,7 @@ resource "google_sql_database_instance" "default" {
     availability_type           = "${var.availability_type}"
     authorized_gae_applications = ["${var.authorized_gae_applications}"]
     backup_configuration        = ["${var.backup_configuration}"]
-    ip_configuration            = ["${var.ip_configuration}"]
+    ip_configuration            = ["${local.ip_configurations["${local.ip_configuration_enabled ? "enabled" : "disabled"}"]}"]
 
     disk_autoresize = "${var.disk_autoresize}"
     disk_size       = "${var.disk_size}"
