b5f2e7864f680e4438d8fe383c446b3914a5be0d
Kunpei Sakai <kunpei@google.com>
2019-08-16 20:19:52 +0900

    fix a typo

--- modules/postgresql/outputs.tf
+++ modules/postgresql/outputs.tf
@@ -20,12 +20,12 @@ output "instance_name" {
   description = "The instance name for the master instance"
 }
 
-output "instance_address" {
+output "instance_ip_address" {
   value       = "${google_sql_database_instance.default.ip_address}"
   description = "The IPv4 addesses assigned for the master instance"
 }
 
-output private_address {
+output "private_address" {
   value       = "${google_sql_database_instance.default.private_ip_address}"
   description = "The private IP address assigned for the master instance"
 }
