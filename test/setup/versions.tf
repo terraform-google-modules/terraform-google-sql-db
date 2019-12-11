a0cf9b28b6fb75a3f2c726b5bd03c043192057ec
omazin <osmansimf@rambler.ru>
2019-12-11 17:37:03 +0300

    [setup] Roll back.

--- test/setup/versions.tf
+++ test/setup/versions.tf
@@ -15,13 +15,13 @@
  */
 
 terraform {
-  required_version = "~> 0.12.12"
+  required_version = ">= 0.12"
 }
 
 provider "google" {
-  version = "~> 2.9.0"
+  version = "~> 2.13.0"
 }
 
 provider "google-beta" {
-  version = "~> 2.9.0"
+  version = "~> 2.13.0"
 }
