6413ea73ea6d431193c85e009b753887d25049b1
Dev <devan2patel@gmail.com>
2020-03-31 13:36:24 +0100

    WIP
    Signed-off-by: Dev <devan2patel@gmail.com>

--- modules/mysql/main.tf
+++ modules/mysql/main.tf
@@ -22,6 +22,9 @@ locals {
     enabled  = var.ip_configuration
     disabled = {}
   }
+
+  databases = { for db in var.additional_databases : db.name => db }
+  users     = { for u in var.additional_users : u.name => u }
 }
 
 resource "google_sql_database_instance" "default" {
@@ -110,11 +113,11 @@ resource "google_sql_database" "default" {
 }
 
 resource "google_sql_database" "additional_databases" {
-  count      = length(var.additional_databases)
+  for_each   = local.databases
   project    = var.project_id
-  name       = var.additional_databases[count.index]["name"]
-  charset    = lookup(var.additional_databases[count.index], "charset", null)
-  collation  = lookup(var.additional_databases[count.index], "collation", null)
+  name       = each.value.name
+  charset    = lookup(each.value, "charset", null)
+  collation  = lookup(each.value, "collation", null)
   instance   = google_sql_database_instance.default.name
   depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
 }
@@ -138,15 +141,11 @@ resource "google_sql_user" "default" {
 }
 
 resource "google_sql_user" "additional_users" {
-  count   = length(var.additional_users)
-  project = var.project_id
-  name    = var.additional_users[count.index]["name"]
-  password = lookup(
-    var.additional_users[count.index],
-    "password",
-    random_id.user-password.hex,
-  )
-  host       = lookup(var.additional_users[count.index], "host", var.user_host)
+  for_each   = local.users
+  project    = var.project_id
+  name       = each.value.name
+  password   = lookup(each.value, "password", random_id.user-password.hex)
+  host       = lookup(each.value, "host", var.user_host)
   instance   = google_sql_database_instance.default.name
   depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
 }
