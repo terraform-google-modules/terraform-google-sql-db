fccb9d39822995862598f3ea8bfe8a65b121c870
Pascal Bourdier <PascalBourdier@users.noreply.github.com>
2020-06-05 15:34:00 +0200

    chore: fix typo in doc for postgres examples (#120)

--- examples/postgresql-ha/main.tf
+++ examples/postgresql-ha/main.tf
@@ -34,7 +34,7 @@ locals {
   /*
     Random instance name needed because:
     "You cannot reuse an instance name for up to a week after you have deleted an instance."
-    See https://cloud.google.com/sql/docs/mysql/delete-instance for details.
+    See https://cloud.google.com/sql/docs/postgres/delete-instance for details.
   */
   instance_name = "${var.pg_ha_name}-${random_id.instance_name_suffix.hex}"
 
