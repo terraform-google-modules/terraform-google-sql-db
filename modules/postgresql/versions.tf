ce37094402ff8546573dd40b75f3a9beaf0ab2b6
Morgante Pell <morgantep@google.com>
2021-03-18 18:41:12 -0400

    Update versions.tf

--- modules/postgresql/versions.tf
+++ modules/postgresql/versions.tf
@@ -17,7 +17,14 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.2"
+    }
     google = {
       source  = "hashicorp/google"
       version = "~> 3.53"
