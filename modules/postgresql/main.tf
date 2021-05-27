f67ca5e0387b86a3be749e1300fbfdefe6b5e436
Ulminator <25484774+Ulminator@users.noreply.github.com>
2021-05-26 22:21:34 -0700

    fix: prevent null from being passed as password

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -172,7 +172,7 @@ resource "google_sql_user" "additional_users" {
   for_each   = local.users
   project    = var.project_id
   name       = each.value.name
-  password   = lookup(each.value, "password", random_id.user-password.hex)
+  password   = coalesce(each.value["password"], random_id.user-password.hex)
   instance   = google_sql_database_instance.default.name
   depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
 }
