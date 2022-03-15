4206f3cbde1424a73d44b535d6c7613b53cbf667
Amir Hadi <amir-hadi@users.noreply.github.com>
2022-03-15 18:00:29 +0100

    Add provider_meta for the google-beta provider

--- modules/postgresql/versions.tf
+++ modules/postgresql/versions.tf
@@ -38,5 +38,8 @@ terraform {
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-sql-db:postgresql/v10.0.0"
   }
+  provider_meta "google-beta" {
+    module_name = "blueprints/terraform/terraform-google-sql-db:postgresql/v10.0.0"
+  }
 
 }
