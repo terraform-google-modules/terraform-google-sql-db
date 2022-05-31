a69c94b1dbb3a15859a2771226009b6ab63bfb79
Clément BUCHART <clement@buchart.dev>
2022-05-31 10:04:43 +0200

    chore: Bump random provider

--- modules/postgresql/versions.tf
+++ modules/postgresql/versions.tf
@@ -23,7 +23,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "~> 3.1"
     }
     google = {
       source  = "hashicorp/google"
