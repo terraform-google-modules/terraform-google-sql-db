aabc68d481aab7837f717adff428d0d9072e8588
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2022-08-11 14:44:55 +0700

    Update unit test with sensitive values

--- examples/mysql-public/outputs.tf
+++ examples/mysql-public/outputs.tf
@@ -30,6 +30,7 @@ output "mysql_conn" {
 }
 
 output "mysql_user_pass" {
+  sensitive   = true
   value       = module.mysql-db.generated_user_password
   description = "The password for the default user. If not set, a random one will be generated and available in the generated_user_password output variable."
 }
