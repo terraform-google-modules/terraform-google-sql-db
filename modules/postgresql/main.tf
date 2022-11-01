e796b3cb44500bcc144fc28fc482111a461ad465
Jean Prat <saez0pub@users.noreply.github.com>
2022-11-01 06:00:02 +0100

    fix: Postgresql availability type shouldn't prevent backup configuration (#352)
    fixes #351
    
    Co-authored-by: Andrew Peabody <andrewpeabody@google.com>
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -32,8 +32,8 @@ locals {
   }]
 
   // HA method using REGIONAL availability_type requires point in time recovery to be enabled
-  point_in_time_recovery_enabled = var.availability_type == "REGIONAL" ? true : lookup(var.backup_configuration, "point_in_time_recovery_enabled", false)
-  backups_enabled                = var.availability_type == "REGIONAL" ? true : lookup(var.backup_configuration, "enabled", false)
+  point_in_time_recovery_enabled = var.availability_type == "REGIONAL" ? lookup(var.backup_configuration, "point_in_time_recovery_enabled", true) : lookup(var.backup_configuration, "point_in_time_recovery_enabled", false)
+  backups_enabled                = var.availability_type == "REGIONAL" ? lookup(var.backup_configuration, "enabled", true) : lookup(var.backup_configuration, "enabled", false)
 
   retained_backups = lookup(var.backup_configuration, "retained_backups", null)
   retention_unit   = lookup(var.backup_configuration, "retention_unit", null)
