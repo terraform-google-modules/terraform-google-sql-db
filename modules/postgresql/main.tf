dc1d8d7181e233a5a0dca13f4797c471c8b244ff
Saurabh Uttam <saurabhuttam@google.com>
2022-12-09 09:27:45 +0000

    Fixed formating issues

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -149,24 +149,24 @@ resource "google_sql_database_instance" "default" {
 }
 
 resource "google_sql_database" "default" {
-  count      = var.enable_default_db ? 1 : 0
-  name       = var.db_name
-  project    = var.project_id
-  instance   = google_sql_database_instance.default.name
-  charset    = var.db_charset
-  collation  = var.db_collation
-  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+  count           = var.enable_default_db ? 1 : 0
+  name            = var.db_name
+  project         = var.project_id
+  instance        = google_sql_database_instance.default.name
+  charset         = var.db_charset
+  collation       = var.db_collation
+  depends_on      = [null_resource.module_depends_on, google_sql_database_instance.default]
   deletion_policy = var.database_deletion_policy
 }
 
 resource "google_sql_database" "additional_databases" {
-  for_each   = local.databases
-  project    = var.project_id
-  name       = each.value.name
-  charset    = lookup(each.value, "charset", null)
-  collation  = lookup(each.value, "collation", null)
-  instance   = google_sql_database_instance.default.name
-  depends_on = [null_resource.module_depends_on, google_sql_database_instance.default]
+  for_each        = local.databases
+  project         = var.project_id
+  name            = each.value.name
+  charset         = lookup(each.value, "charset", null)
+  collation       = lookup(each.value, "collation", null)
+  instance        = google_sql_database_instance.default.name
+  depends_on      = [null_resource.module_depends_on, google_sql_database_instance.default]
   deletion_policy = var.database_deletion_policy
 }
 
