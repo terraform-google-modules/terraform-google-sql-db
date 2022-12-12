34f187b19ea4d5e3272395fc7c2422f0f2bf6b4a
ravisiddhu <ravisiddharth@google.com>
2022-12-12 13:23:44 +0000

    Added functionality to simply specify the database version number instead of the whole version name for postgres module.

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -49,7 +49,7 @@ resource "google_sql_database_instance" "default" {
   provider            = google-beta
   project             = var.project_id
   name                = local.master_instance_name
-  database_version    = (length(var.database_version) >= 8 && (substr(var.database_version, 0, 8) == "POSTGRES" || substr(var.database_version, 0, 8) == "postgres")) ? var.database_version : format("POSTGRES_%s", var.database_version)
+  database_version    = can(regex("\\d", substr(var.database_version, 0, 1))) ? format("POSTGRES_%s", var.database_version) : var.database_version
   region              = var.region
   encryption_key_name = var.encryption_key_name
   deletion_protection = var.deletion_protection
