4d7a92af26df1fb9a88631d925ffb9f939e55a20
Dan <daniel.feely@newswhip.com>
2023-06-27 18:23:07 +0100

    Change back to master

--- outputs.tf
+++ outputs.tf
@@ -16,22 +16,22 @@
 
 output instance_name {
   description = "The name of the database instance"
-  value       = "${google_sql_database_instance.master.name}"
+  value       = "${google_sql_database_instance.default.name}"
 }
 
 output instance_address {
   description = "The IPv4 address of the master database instance"
-  value       = "${google_sql_database_instance.master.ip_address.0.ip_address}"
+  value       = "${google_sql_database_instance.default.ip_address.0.ip_address}"
 }
 
 output instance_address_time_to_retire {
   description = "The time the master instance IP address will be retired. RFC 3339 format."
-  value       = "${google_sql_database_instance.master.ip_address.0.time_to_retire}"
+  value       = "${google_sql_database_instance.default.ip_address.0.time_to_retire}"
 }
 
 output self_link {
   description = "Self link to the master instance"
-  value       = "${google_sql_database_instance.master.self_link}"
+  value       = "${google_sql_database_instance.default.self_link}"
 }
 
 output generated_user_password {
