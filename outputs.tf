b06f8d9cb84cfefa547a124b8828c1377d73b02c
Dan <daniel.feely@newswhip.com>
2023-06-27 18:20:24 +0100

    Typos

--- outputs.tf
+++ outputs.tf
@@ -20,12 +20,12 @@ output instance_name {
 }
 
 output instance_address {
-  description = "The IPv4 address of the master database instnace"
+  description = "The IPv4 address of the master database instance"
   value       = "${google_sql_database_instance.master.ip_address.0.ip_address}"
 }
 
 output instance_address_time_to_retire {
-  description = "The time the master instance IP address will be reitred. RFC 3339 format."
+  description = "The time the master instance IP address will be retired. RFC 3339 format."
   value       = "${google_sql_database_instance.master.ip_address.0.time_to_retire}"
 }
 
