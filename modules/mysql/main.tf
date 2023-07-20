7c3ff6661200336ab0a95c495d10f0fe585fa10e
ibhaskar2 <ibhaskar@google.com>
2023-07-20 11:42:30 +0000

    Removed map lookup from data_cache_config dynamic block

--- modules/mysql/main.tf
+++ modules/mysql/main.tf
@@ -98,7 +98,7 @@ resource "google_sql_database_instance" "default" {
     dynamic "data_cache_config" {
       for_each = var.edition == "ENTERPRISE_PLUS" && var.data_cache_enabled ? ["cache_enabled"] : []
       content {
-        data_cache_enabled = lookup(data_cache_config.value, "data_cache_enabled", false)
+        data_cache_enabled = var.data_cache_enabled
       }
     }
     dynamic "deny_maintenance_period" {
