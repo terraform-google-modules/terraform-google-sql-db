34436d3bb41718fd82d29a8dbbda999197a2aeea
Dan <daniel.feely@newswhip.com>
2023-09-25 18:30:14 +0100

    fmt

--- main.tf
+++ main.tf
@@ -22,17 +22,16 @@ resource "google_sql_database_instance" "default" {
   master_instance_name = "${var.master_instance_name}"
 
   settings {
-    tier                        = "${var.tier}"
-    activation_policy           = "${var.activation_policy}"
-    authorized_gae_applications = ["${var.authorized_gae_applications}"]
-    disk_autoresize             = "${var.disk_autoresize}"
-    backup_configuration        = ["${var.backup_configuration}"]
-    ip_configuration            = ["${var.ip_configuration}"]
-    location_preference         = ["${var.location_preference}"]
-    disk_size                   = "${var.disk_size}"
-    disk_type                   = "${var.disk_type}"
-    pricing_plan                = "${var.pricing_plan}"
-    user_labels                 = var.labels
+    tier                 = "${var.tier}"
+    activation_policy    = "${var.activation_policy}"
+    disk_autoresize      = "${var.disk_autoresize}"
+    backup_configuration = ["${var.backup_configuration}"]
+    ip_configuration     = ["${var.ip_configuration}"]
+    location_preference  = ["${var.location_preference}"]
+    disk_size            = "${var.disk_size}"
+    disk_type            = "${var.disk_type}"
+    pricing_plan         = "${var.pricing_plan}"
+    user_labels          = var.labels
 
     dynamic database_flags {
       for_each = var.database_flags
