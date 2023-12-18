9535ada62b834d81701d44c534e74992f4366811
bharathkkb <bharathkrishnakb@gmail.com>
2023-12-18 21:20:37 +0000

    chore: update examples to use registry

--- examples/mssql-public/main.tf
+++ examples/mssql-public/main.tf
@@ -15,7 +15,9 @@
  */
 
 module "mssql" {
-  source               = "../../modules/mssql"
+  source  = "terraform-google-modules/sql-db/google//modules/mssql"
+  version = "~> 18.0"
+
   name                 = var.name
   random_instance_name = true
   project_id           = var.project_id
