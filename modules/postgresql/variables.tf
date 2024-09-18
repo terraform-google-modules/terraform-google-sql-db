9036b3339890caa07c071a49871254758f7c9f8c
Zheng Qin <zhengqin@google.com>
2024-09-18 16:20:31 -0400

    Update defaults for postgresql module

--- modules/postgresql/variables.tf
+++ modules/postgresql/variables.tf
@@ -69,7 +69,7 @@ variable "tier" {
 variable "edition" {
   description = "The edition of the Cloud SQL instance, can be ENTERPRISE or ENTERPRISE_PLUS."
   type        = string
-  default     = null
+  default     = "ENTERPRISE_PLUS"
 }
 
 variable "zone" {
@@ -105,7 +105,7 @@ variable "availability_type" {
 variable "deletion_protection_enabled" {
   description = "Enables protection of an Cloud SQL instance from accidental deletion across all surfaces (API, gcloud, Cloud Console and Terraform)."
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "read_replica_deletion_protection_enabled" {
@@ -168,7 +168,12 @@ variable "database_flags" {
     name  = string
     value = string
   }))
-  default = []
+  default = [
+    {
+      name = "cloudsql.iam_authentication",
+      value = "true"
+    }
+  ]
 }
 
 variable "user_labels" {
@@ -190,10 +195,10 @@ variable "deny_maintenance_period" {
 variable "backup_configuration" {
   description = "The backup_configuration settings subblock for the database setings"
   type = object({
-    enabled                        = optional(bool, false)
+    enabled                        = optional(bool, true)
     start_time                     = optional(string)
     location                       = optional(string)
-    point_in_time_recovery_enabled = optional(bool, false)
+    point_in_time_recovery_enabled = optional(bool, true)
     transaction_log_retention_days = optional(string)
     retained_backups               = optional(number)
     retention_unit                 = optional(string)
@@ -436,7 +441,7 @@ variable "root_password" {
 variable "data_cache_enabled" {
   description = "Whether data cache is enabled for the instance. Defaults to false. Feature is only available for ENTERPRISE_PLUS tier and supported database_versions"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "enable_google_ml_integration" {
