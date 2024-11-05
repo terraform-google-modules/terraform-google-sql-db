3e661257a78144ce6d20a1dd988a36ce73ccde95
Imran Nayer <imrannayer@google.com>
2024-11-05 03:21:46 +0000

    fixed issue 634

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -137,14 +137,14 @@ resource "google_sql_database_instance" "default" {
       }
     }
     dynamic "insights_config" {
-      for_each = var.insights_config != null ? [var.insights_config] : []
+      for_each = var.insights_config != null ? [var.insights_config] : ["1"]
 
       content {
-        query_insights_enabled  = true
-        query_plans_per_minute  = lookup(insights_config.value, "query_plans_per_minute", 5)
-        query_string_length     = lookup(insights_config.value, "query_string_length", 1024)
-        record_application_tags = lookup(insights_config.value, "record_application_tags", false)
-        record_client_address   = lookup(insights_config.value, "record_client_address", false)
+        query_insights_enabled  = var.insights_config != null ? true : false
+        query_plans_per_minute  = var.insights_config != null ? lookup(insights_config.value, "query_plans_per_minute", 5) : null
+        query_string_length     = var.insights_config != null ? lookup(insights_config.value, "query_string_length", 1024) : null
+        record_application_tags = var.insights_config != null ? lookup(insights_config.value, "record_application_tags", false) : null
+        record_client_address   = var.insights_config != null ? lookup(insights_config.value, "record_client_address", false) : null
       }
     }
 
