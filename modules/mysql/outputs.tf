e402719527f0154bdd192b00cf87743a48b1a98d
amrkk123 <ajaykulkarni4projects@gmail.com>
2025-02-12 05:57:32 +0000

    Rechecking all the fields

--- modules/mysql/outputs.tf
+++ modules/mysql/outputs.tf
@@ -158,8 +158,7 @@ output "env_vars" {
 output "apphub_service_uri" {
   value = {
     service_uri = "//cloudsql.googleapis.com/projects${element(split("/projects", google_sql_database_instance.default.self_link), 1)}"
-    service_id  = substr("${var.service_name}-${md5("${var.location}-${var.project_id}")}", 0, 63)
-    location    = var.location
+    service_id  = substr(format("%s-%s", var.name, md5(var.project_id)), 0, 63)
   }
   description = "Service URI in CAIS style to be used by Apphub."
 }
