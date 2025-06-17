d0b31dd44d23465df72a91d50755d1209e6c4bdc
Oscar van Leusen <oscar.vanleusen@pexip.com>
2025-06-17 19:07:25 +0100

    feat(TPG>6.31)!: Support retain_backups_on_delete for mysql, safer_mysql (#735)
    Co-authored-by: Imran Nayer <imrannayer@google.com>

--- modules/safer_mysql/main.tf
+++ modules/safer_mysql/main.tf
@@ -50,7 +50,8 @@ module "safer_mysql" {
 
   user_labels = var.user_labels
 
-  backup_configuration = var.backup_configuration
+  backup_configuration     = var.backup_configuration
+  retain_backups_on_delete = var.retain_backups_on_delete
 
   insights_config = var.insights_config
 
