9040d89456031aceaa51ac4ac2f7014e6aced6d5
Oscar van Leusen <oscar.vanleusen@pexip.com>
2025-06-20 16:55:40 +0100

    Add support for psc_auto_connections feature to mysql and postgres

--- modules/postgresql/variables.tf
+++ modules/postgresql/variables.tf
@@ -330,9 +330,13 @@ variable "ip_configuration" {
     enable_private_path_for_google_cloud_services = optional(bool, false)
     psc_enabled                                   = optional(bool, false)
     psc_allowed_consumer_projects                 = optional(list(string), [])
-    server_ca_mode                                = optional(string)
-    server_ca_pool                                = optional(string)
-    custom_subject_alternative_names              = optional(list(string), [])
+    psc_auto_connections = optional(list(object({
+      consumer_network            = string
+      consumer_service_project_id = string
+    })), [])
+    server_ca_mode                   = optional(string)
+    server_ca_pool                   = optional(string)
+    custom_subject_alternative_names = optional(list(string), [])
   })
   default = {}
 }
