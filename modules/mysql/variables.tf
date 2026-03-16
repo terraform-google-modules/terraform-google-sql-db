315157c486d4856dd50abb34c47245f53fc14125
Dan Cahoon <dpcahoon@ginkgobioworks.com>
2026-03-16 10:42:50 -0400

    Fix copy/paste

--- modules/mysql/variables.tf
+++ modules/mysql/variables.tf
@@ -423,7 +423,7 @@ variable "read_replicas" {
       enable_private_path_for_google_cloud_services = optional(bool, false)
       psc_enabled                                   = optional(bool, false)
       psc_allowed_consumer_projects                 = optional(list(string), [])
-      psc_allowed_consumer_projects                 = optional(list(string), [])
+      psc_network_attachment_uri                    = optional(string)
     })
     encryption_key_name = optional(string)
     data_cache_enabled  = optional(bool)
